2ff791233ef219bdc407c40c64f2611c5f3464e0
ya-bystritskiy <bystritskii.yaroslav@gmail.com>
2024-06-25 22:38:45 +0300

    commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,36 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
     
 
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+    }
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "c", ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    
 }
 
